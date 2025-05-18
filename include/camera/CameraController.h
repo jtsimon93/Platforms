7c1d73688a8ca5bf5b6569b584175881bd315c9c
Justin Simon <jtsimon@jtsimon.dev>
2025-05-18 12:16:17 -0400

    Formatting

--- include/camera/CameraController.h
+++ include/camera/CameraController.h
@@ -1,19 +1,17 @@
 #pragma once
 
-extern "C"
-{
+extern "C" {
 #include "raylib.h"
 }
 
-class CameraController
-{
+class CameraController {
 public:
-    void Init(float screenWidth, float screenHeight, float zoom = 2.0f);
-    void Update(const Vector2 &targetPosition);
-    void Begin() const;
-    void End() const;
-    const Camera2D &GetRaylibCamera() const;
+  void Init(float screenWidth, float screenHeight, float zoom = 2.0f);
+  void Update(const Vector2 &targetPosition);
+  void Begin() const;
+  void End() const;
+  const Camera2D &GetRaylibCamera() const;
 
 private:
-    Camera2D camera;
+  Camera2D camera;
 };
